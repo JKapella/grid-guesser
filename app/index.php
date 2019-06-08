dbf30ca0ab339b6f11978276c4c6898f74de47ef
Jkapella <jvkapella@gmail.com>
2019-06-08 11:29:31 +0100

    added values to form

--- app/index.php
+++ app/index.php
@@ -13,7 +13,8 @@
         <h3>Choose your Grid size and hit 'go' to play!</h3>
         <p>(Each side can be between 2 and 100!)</p>
         <form action='game.php' method='post'>
-            <input type='text'> squares wide and <input type='text'> squares high
+            <input type='text' name='widthValue'> squares wide and
+            <input type='text' name='heightValue'> squares high
             <div id='errorField'></div>
             <input type='submit'>
         </form>
