4c3e3ff8f42506c608fd17ce83b6b65899dede90
Jkapella <jvkapella@gmail.com>
2019-06-08 11:03:26 +0100

    created game php file, improved semantic tags on section file

---
+++ game.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<head>
+    <meta charset="utf-8">
+    <title>Grid Guesser</title>
+</head>
+
+<body>
+<h1>Grid Guesser</h1>
+<main>
+    <h3>Select a square!</h3>
+    <main></main>
+</main>
+</body>
--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <meta charset="utf-8">
     <title>Grid Guesser</title>
 </head>
+
 <body>
     <h1>Grid Guesser</h1>
-    <main>
+    <section>
         <h3>Choose your Grid size and hit 'go' to play!</h3>
         <p>(Each side can be between 2 and 100!)</p>
         <form action=''>
             <input type='text'> squares wide and <input type='text'> squares high
+            <div id='errorField'></div>
             <input type='submit'>
         </form>
-    </main>
+    </section>
 </body>
