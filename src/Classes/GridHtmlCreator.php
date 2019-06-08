2b7a2d755fd1a84e3e437e0f82e1746ae774a24e
Jkapella <jvkapella@gmail.com>
2019-06-08 14:44:02 +0100

    added prelimary styles for debugging

--- src/Classes/GridHtmlCreator.php
+++ src/Classes/GridHtmlCreator.php
@@ -14,12 +14,14 @@ class GridHtmlCreator
 
         $output = '';
         for ($y = 1; $y <= $yRows; $y++) {
+            $output .= "<div class='grid-row'>";
             for ($x = 1; $x <= $xRows; $x++) {
                 $yDist = abs($y - $winY);
                 $xDist = abs($x - $winX);
                 $distance = abs($yDist - $xDist);
-                $output .= "<div class='grid-square' data-dist='" . $distance . "'></div>";
+                $output .= "<div class='grid-square' data-dist='" . $distance . "'>X</div>";
             }
+            $output .= "</div>";
         }
 
         return $output;
