66df02d6a4d46a106760c7dd43c466c679526e65
Jkapella <jvkapella@gmail.com>
2019-06-08 10:51:30 +0100

    added rough html layout for grid select screen

--- index.php
+++ index.php
@@ -7,5 +7,13 @@
     <title>Grid Guesser</title>
 </head>
 <body>
-
+    <h1>Grid Guesser</h1>
+    <main>
+        <h3>Choose your Grid size and hit 'go' to play!</h3>
+        <p>(Each side can be between 2 and 100!)</p>
+        <form action=''>
+            <input type='text'> x <input type='text'> squares
+            <input type='submit'>
+        </form>
+    </main>
 </body>
