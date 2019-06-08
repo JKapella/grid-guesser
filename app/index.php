483cdb9b187f40de9b95942c497d53565b197ad4
Jkapella <jvkapella@gmail.com>
2019-06-08 15:32:29 +0100

    added html length limit to form on index

--- app/index.php
+++ app/index.php
@@ -13,10 +13,10 @@
     <h1>Grid Guesser</h1>
     <section>
         <h3>Choose your Grid size and hit 'go' to play!</h3>
-        <p>(Each side can be between 2 and 100!)</p>
+        <p>(Each side can be between 2 and 50!)</p>
         <form action='game.php' method='post'>
-            <input type='text' name='widthValue'> squares wide and
-            <input type='text' name='heightValue'> squares high
+            <input type='text' name='widthValue' maxlength='2' size='2'> squares wide and
+            <input type='text' name='heightValue' maxlength='2' size='2'> squares high
             <div id='errorField'></div>
             <input type='submit'>
         </form>
