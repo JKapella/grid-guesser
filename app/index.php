8648962940b192d3ee0303078c3d9ba5a44009fc
Jkapella <jvkapella@gmail.com>
2019-06-08 16:46:51 +0100

    added validation for index form

--- app/index.php
+++ app/index.php
@@ -16,8 +16,8 @@
         <h3>Choose your Grid size and hit 'go' to play!</h3>
         <p>(Each side can be between 2 and 50!)</p>
         <form id='form' action='game.php' method='post'>
-            <input type='text' name='widthValue' maxlength='2' size='2'> squares wide and
-            <input type='text' name='heightValue' maxlength='2' size='2'> squares high
+            <input id='width' type='text' name='widthValue' maxlength='2' size='2'> squares wide and
+            <input id='height' type='text' name='heightValue' maxlength='2' size='2'> squares high
             <div id='errorField'></div>
             <input type='submit'>
         </form>
--- app/js/validation.js
+++ app/js/validation.js
@@ -7,7 +7,16 @@ function addFormListener() {
 }
 
 function validateForm() {
-    
+    let height = document.getElementById('height').value
+    let width = document.getElementById('width').value
+    if (width < 2 || width > 50 ||
+        height < 2 || height > 50 ||
+        isNaN(width) || isNaN(height)) {
+        let errorField = document.getElementById('errorField')
+        errorField.textContent = 'Width and Height must be numbers between 2 and 50!'
+    } else {
+        document.getElementById('form').submit()
+    }
 }
 
 addFormListener()
