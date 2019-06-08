ab8a2aa47ae6ac7feb9cc77f662f0b68a8bcdef1
Jkapella <jvkapella@gmail.com>
2019-06-08 11:38:44 +0100

    added validation to game.php file, redirects to index if invalid

--- app/game.php
+++ app/game.php
@@ -1,5 +1,17 @@
 <?php
 
+if (isset($_POST['widthValue']) && isset($_POST['heightValue'])) {
+    $requestedWidth =  $_POST['widthValue'];
+    $requestedHeight = $_POST['heightValue'];
+    if ($requestedWidth > 1 && $requestedHeight > 1) {
+        //get a grid
+    } else {
+        header('Location: index.php');
+    }
+} else {
+    header('Location: index.php');
+}
+
 ?>
 
 <head>
